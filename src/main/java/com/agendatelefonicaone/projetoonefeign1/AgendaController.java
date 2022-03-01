3ddf96cc0f58d69ed1500432beea34aac0d94b2d
Claudio Francisco das Neves <cfneguacu@uni9.edu.br>
2022-03-01 16:34:46 -0300

    Projeto Feign Agenda Telefonica Controller Principal

---
+++ src/main/java/com/agendatelefonicaone/projetoonefeign1/AgendaController.java
@@ -0,0 +1,19 @@
+package com.agendatelefonicaone.projetoonefeign1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("contato")
+public class AgendaController {
+
+    @GetMapping
+    public Contato retornaContato(){
+        return Contato.builder()
+                .id(1L)
+                .nome("Cláudio")
+                .telefone("962669041")
+                .build();
+    }
+}
---
+++ src/test/java/com/agendatelefonicaone/projetoonefeign1/Projetoonefeign1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agendatelefonicaone.projetoonefeign1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projetoonefeign1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
